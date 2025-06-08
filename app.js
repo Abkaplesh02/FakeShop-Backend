066b51e447655d1d1792cd4b5d857ead19a6243c
Abkaplesh02 <abkaplesh02@gmail.com>
2025-06-09 01:34:59 +0530

    update cors erro

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://fakeshopmern.netlify.app/",
+    origin:"https://fakeshopmern.netlify.app",
     credentials:true,
 }))
 
